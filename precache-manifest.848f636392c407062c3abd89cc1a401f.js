a5c1b45599b3905468b965a1690ccf1c251f3691
Maciej Kotkowski <maciej.kotkowski@flokk.com>
2023-08-28 15:29:48 +0200

    Updates

---
+++ precache-manifest.848f636392c407062c3abd89cc1a401f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0bfe6499eb5819ff3c3",
+    "url": "/rolling-up/static/css/main.e01372ec.chunk.css"
+  },
+  {
+    "revision": "c0bfe6499eb5819ff3c3",
+    "url": "/rolling-up/static/js/main.ed4a6779.chunk.js"
+  },
+  {
+    "revision": "bdfc919f63c43600224b",
+    "url": "/rolling-up/static/js/runtime~main.8224473e.js"
+  },
+  {
+    "revision": "4d363ce02462e7761024",
+    "url": "/rolling-up/static/js/2.4766a264.chunk.js"
+  },
+  {
+    "revision": "52395e38ea8e8dac02e1",
+    "url": "/rolling-up/static/js/3.af43732f.chunk.js"
+  },
+  {
+    "revision": "ecdf0b57d2caed374780a059efa8d261",
+    "url": "/rolling-up/index.html"
+  }
+];
